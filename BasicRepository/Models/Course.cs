e63a90766ca54b5b7e7a97021dd78af80f0c21ba
mccart84 <ryan.mccarty90@hotmail.com>
2016-02-21 22:10:23 -0500

    changedTracking

---
+++ BasicRepository/Models/Course.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicRepository.Models
+{
+    public class Course
+    {
+        #region [FIELDS]
+        private int _id;
+        private string _name;
+        private string _address;
+        private string _city;
+        private StateEnum.StateAbrv _state;
+        private string _zip;
+        private bool _open;
+        private DateTime _timeStamp;
+        #endregion
+
+        #region [PROPERTIES]
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public StateEnum.StateAbrv State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public string Zip
+        {
+            get { return _zip; }
+            set { _zip = value; }
+        }
+
+        public bool Open
+        {
+            get { return _open; }
+            set { _open = value; }
+        }
+
+        public DateTime TimeStamp
+        {
+            get { return _timeStamp; }
+            set { _timeStamp = value; }
+        }
+        #endregion
+    }
+}
